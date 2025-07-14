ce0cd941a3c1b5e404572f83531f8fcbd28fe44d
Revaz Nakhutsrishvili <r@revaz.xyz>
2025-07-14 13:03:00 +0400

    feat(users): update create dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @MinLength(6)
+  readonly password: string;
+}
